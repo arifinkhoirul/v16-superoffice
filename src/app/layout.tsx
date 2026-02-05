5bb5704b142c97f4b7b10e0d09ea28fb44724ae7
arifinkhoirul <irul30582@gmail.com>
2026-02-05 22:37:20 +0700

    commit anlytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from '@vercel/analytics/next';
 
 
 const geistSans = Geist({
@@ -64,6 +64,7 @@ export default function RootLayout({
         <Navbar/>
 
           {children}
+          <Analytics />
       </body>
     </html>
   );
